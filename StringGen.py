11e2268d518d1a859b5ba7a6ac8949d9ca747e92
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-21 09:55:43 +0530

    Update StringGen.py

--- StringGen.py
+++ StringGen.py
@@ -210,5 +210,45 @@ async def genstr(_, message):
         await app.send_message(chat.id, f"**ERROR:** `{str(e)}`")
         return
 
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from StringGen.filters import command, other_filters, other_filters2
+
+
+@Client.on_message(command("start") & other_filters2)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""🙃 Hi {message.from_user.first_name}!
+✨ Hey I am DaisyX String Generator Bot. 
+🥳 I can Generator String Session for You 😉
+⚜️ Use these buttons below to know more. 👇
+🔥 Source Code Made by Devil With Help Of Other Bots 🔥
+👉 Type /genstr for Generating String Session 👈""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⚒ Source code", url="https://github.com/SkemTools"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "💬 Group", url="https://t.me/DaisySupport_Official"
+                    ),
+                    InlineKeyboardButton(
+                        "Channel 🔈", url="https://t.me/DaisyXUpdates"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "❌ Close ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
+
+
 
 
