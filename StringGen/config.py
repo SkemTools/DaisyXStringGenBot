92cf5970b9d5c529c64e2c97b2a0ceebab5b70b8
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-21 09:48:04 +0530

    Create config.py

---
+++ StringGen/config.py
@@ -0,0 +1,17 @@
+# © Copyright Devil ( @lucifeermorningstar ) 
+# Made By Devil ❤
+
+import os
+
+# Change Accordingly While Deploying To A VPS
+
+APP_ID = int(os.environ.get("APP_ID"))
+
+API_HASH = os.environ.get("API_HASH")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+BOT_NAME = os.environ.get("BOT_NAME")  
+
+COMMAND_PREFIXES = os.environ.get("COMMAND_PREFIXES") 
+
